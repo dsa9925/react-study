5f5a43e6cf2903ed04b424ed4d9d5db712800e45
kimdohyun <dev.dsa9925@gmail.com>
2023-11-16 12:38:18 +0900

    docs : 컴포넌트 구별하여 생성(Header.js/Footer.js)

---
+++ src/components/Header.js
@@ -0,0 +1,85 @@
+const Header = () => {
+  return (
+    <div>
+      {/* 상단 내용 고정 */}
+      <header>
+        <div>
+          <a href="#">로고</a>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Menu-1</a>
+                <ul>
+                  <li>
+                    <a href="#">SubMenu-1</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-2</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-3</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Menu-2</a>
+                <ul>
+                  <li>
+                    <a href="#">SubMenu-1</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-2</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-3</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Menu-3</a>
+                <ul>
+                  <li>
+                    <a href="#">SubMenu-1</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-2</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-3</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Menu-4</a>
+                <ul>
+                  <li>
+                    <a href="#">SubMenu-1</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-2</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-3</a>
+                  </li>
+                  <li>
+                    <a href="#">SubMenu-4</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+export default Header;
