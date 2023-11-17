7ecd1f4932d62d7954a45dffffa917cdc2beb308
kimdohyun <dev.dsa9925@gmail.com>
2023-11-17 18:04:08 +0900

    docs : scss의 이해

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import styles from "../styles/Header.module.css";
 const Header = ({ children }) => {
-  // 내부 html을 전달 받아서 내용을 변경한다.
-  //   const children = props.children;
+  // 내부 html을 전달받아서 내용을 변경한다.
+  // const children = props.children;
   return (
-    <header className={styles.wrapnp}>
+    <header>
       <div>{children}</div>
     </header>
   );
 };
+
 export default Header;
