7b173534de4bd2f0f039e41b6ef6474871315817
kimdohyun <dev.dsa9925@gmail.com>
2023-11-23 16:52:59 +0900

    docs : React Router 이해 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = ({ children, version }) => {
+const Header = () => {
   return (
-    <header>
-      {children} ({version})
-    </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Header</h1>
+      <div style={{ background: "white", textAlign: "center", fontSize: 20 }}>
+        <Link to="/home">Home</Link>|<Link to="/member">Member</Link>|
+        <Link to="/about">About</Link>
+        <Link to="http://www.naver.com" target="_blank" rel="noreferrer">
+          |네이버
+        </Link>
+      </div>
+    </div>
   );
 };
 
