60ceed7210e2e994ef2172aa24229c3ae09d1cf6
dsa9925 <dev.dsa9925@gmail.com>
2024-02-20 13:09:44 +0900

    리액트 타입스크립트 기초

---
+++ src/test.ts
@@ -0,0 +1,25 @@
+[
+  {
+    ishop: 1,
+    name: "고기굽는남자",
+    location: "삼덕동1가 32-10",
+    count: 33,
+    pics: [
+      "22f6a12a-63d9-4c96-a55d-a297d7e2083f.jpg",
+      "28bc248f-873d-44ac-bf31-a59fa7be485a.jpg",
+      "0d9969ca-dde8-4d42-9b10-3caa87e1aca0.jpg",
+    ],
+    facilities: ["단체 가능", "예약 가능", "화장실구분"],
+  },
+  {
+    ishop: 2,
+    name: "실비소갈비",
+    location: "삼덕동2가 132",
+    count: 33,
+    pics: [
+      "0d1cedd2-b347-4772-ac4f-d00f9346a040.jpg",
+      "7a902bcb-45b5-4736-a475-24e1d2247c4e.jpg",
+    ],
+    facilities: ["주차장", "단체 가능", "와이파이", "화장실구분"],
+  },
+];
